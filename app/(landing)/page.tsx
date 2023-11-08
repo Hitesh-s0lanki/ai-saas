55f9b1251675a2122a1b3d3968558c8fda46a4a6
Hitesh Solanki <hiteshsolanki4623@gmail.com>
2023-11-08 17:03:23 +0530

    change

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
-      <LandingContent />
+      {/* <LandingContent /> */}
     </div>
    );
 }
